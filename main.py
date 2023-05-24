7751ec604a4c5fedf5ec1bf36239c747fbd1b5df
Alejandro Skalany <alskalany@gmail.com>
2023-05-23 21:25:58 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+hellodocker = Flask(__name__)
+@hellodocker.route("/")
+
+def run():
+    return "{\"message\":\"Hey there docker\"}"
+
+if __name__ == "__main__":
+    hellodocker.run(host="0.0.0.0", port=int("3000"), debug=True)
+
+
+# to build image
+# terminal -- docker build -t username/filename:versioninformation .
