2af157b4e7a32a2b6fef4705216523e98f723736
Alejandro Skalany <alskalany@gmail.com>
2023-06-01 14:57:33 -0500

    adding dog age calculator, method not allowed error

--- app.py
+++ app.py
@@ -4,19 +4,27 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 
 @app.route('/', methods=['GET','POST'])
-
 def home():
     return render_template('home.html')
 
 @app.route('/surprise', methods=['GET','POST'])
-
 def surprise():
     if request.method == 'POST':
         return render_template('open.html')
 
+@app.route('/age', methods=['POST'])
+def age():
+    return render_template('age.html')
 
-#if __name__ == "__main__":
-#    app.run(debug=True)
+@app.route('/ageresult', methods=['POST'])
+def ageresult():
+    if request.method == 'POST':
+        age_input = request.form['age']
+        result = float(age_input) / 7
+        return render_template('ageresult.html', age_input=age_input, result=result, calculation_success=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 # to build image
 # terminal -- docker build -t username/filename:versioninformation .
