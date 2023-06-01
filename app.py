297706e8bddc847d3a96c0e78ae8652ac3ec2877
Alejandro Skalany <alskalany@gmail.com>
2023-06-01 17:38:04 -0500

    functioning dog age calculator, no consideration for errors added yet

--- app.py
+++ app.py
@@ -12,16 +12,24 @@ def surprise():
     if request.method == 'POST':
         return render_template('open.html')
 
-@app.route('/age', methods=['POST'])
+@app.route('/age', methods=['GET', 'POST'])
 def age():
     return render_template('age.html')
 
-@app.route('/ageresult', methods=['POST'])
+@app.route('/dogyears', methods=['GET', 'POST'])
 def ageresult():
+    ageinput = request.form['age']
     if request.method == 'POST':
-        age_input = request.form['age']
-        result = float(age_input) / 7
-        return render_template('ageresult.html', age_input=age_input, result=result, calculation_success=True)
+        input1 = int(ageinput)
+        result = round(float(input1 / 7))
+        return render_template(
+            'dogyears.html',
+            input1=input1,
+            result=result,
+            calculation_success=True
+        )
+    else:
+        return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
